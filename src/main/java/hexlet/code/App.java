9a11ae6f6372c0bcb9dbbc89eaf127026127ab13
Dinara.Aleskarova <aleskarova@gmail.com>
2021-05-26 13:43:07 +0400

    Singleton scanner, added menu of user choices

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,7 +2,18 @@ package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greetUser();
+        String gameNumber = getGameNumber();
+        if ("1".equals(gameNumber.trim())) {
+            System.out.println("Welcome to the Brain Games!");
+            Cli.greetUser();
+        }
+    }
+
+    private static String getGameNumber() {
+        System.out.println("Please enter the game number and press Enter");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        return UserInputScanner.getScanner().nextLine();
     }
 }
--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -1,13 +1,10 @@
 package hexlet.code;
 
-import java.util.Scanner;
-
 public class Cli {
 
     public static void greetUser() {
         System.out.print("May I have your name? ");
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
+        String name = UserInputScanner.getScanner().nextLine();
         System.out.println("Hello, " + name + "!");
     }
 }
